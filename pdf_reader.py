1ca06385384f636096478624335720f8d7a7b226
krenil-personal <krenilbhikadiya2000@gmail.com>
2026-01-17 15:27:47 +0530

    create pdf reader tool

--- pdf_reader.py
+++ pdf_reader.py
@@ -4,9 +4,30 @@ import PyPDF2
 from langchain_core.tools import tool
 
 
-url = "https://arxiv.org/pdf/1508.02986v1"
-response = requests.get(url)
-pdf_file = io.BytesIO(response.content)
-pdf_reader = PyPDF2.PdfReader(pdf_file)
+@tool
+def read_pdf(url: str) -> str:
+    """
+    Docstring for read_pdf
+    
+    :param url: The URL of the PDF file to read
+    :type url: str
+    :return: The extracted text from the PDF
+    :rtype: str
+    """
+    try:
+        response = requests.get(url)
+        # Load PDF into memory
+        pdf_file = io.BytesIO(response.content)
+        # Read PDF
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        total_pages = len(pdf_reader.pages)
+        text = ""
 
-print(len(pdf_reader.pages))
+        for idx, page in enumerate(pdf_reader.pages, start=1):
+            print(f"Reading page {idx}/{total_pages}")
+            text += page.extract_text() + "\n"
+        
+        return text.strip()
+    except Exception as e:
+        print(f"Error reading PDF: {e}")
+        raise
